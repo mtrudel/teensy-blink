e468172dcb61b6b2910a35e21f843d37b0cca1bb
Mat Trudel <mat@geeky.net>
2019-03-23 11:41:26 -0400

    Hello world rainbow app

--- main.cpp
+++ main.cpp
@@ -1,58 +1,103 @@
-/* Teensyduino Core Library
- * http://www.pjrc.com/teensy/
- * Copyright (c) 2017 PJRC.COM, LLC.
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * 1. The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * 2. If the Software is incorporated into a build system that allows
- * selection among a list of target devices, then similar target
- * devices manufactured by PJRC.COM must be included in the list of
- * target devices and selectable in the same manner.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+#include <OctoWS2811.h>
 
-#include "WProgram.h"
+const int ledsPerStrip = 60;
 
-extern "C" int main(void)
+DMAMEM int displayMemory[ledsPerStrip*6];
+int drawingMemory[ledsPerStrip*6];
+
+const int config = WS2811_GRB | WS2811_800kHz;
+
+OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
+
+int rainbowColors[180];
+
+unsigned int h2rgb(unsigned int v1, unsigned int v2, unsigned int hue)
 {
-#ifdef USING_MAKEFILE
-
-	// To use Teensy 3.0 without Arduino, simply put your code here.
-	// For example:
-
-	pinMode(13, OUTPUT);
-	while (1) {
-		digitalWriteFast(13, HIGH);
-		delay(50);
-		digitalWriteFast(13, LOW);
-		delay(50);
-	}
-
-
-#else
-	// Arduino's main() function just calls setup() and loop()....
-	setup();
-	while (1) {
-		loop();
-		yield();
-	}
-#endif
+	if (hue < 60) return v1 * 60 + (v2 - v1) * hue;
+	if (hue < 180) return v2 * 60;
+	if (hue < 240) return v1 * 60 + (v2 - v1) * (240 - hue);
+	return v1 * 60;
 }
 
+
+int makeColor(unsigned int hue, unsigned int saturation, unsigned int lightness)
+{
+	unsigned int red, green, blue;
+	unsigned int var1, var2;
+
+	if (hue > 359) hue = hue % 360;
+	if (saturation > 100) saturation = 100;
+	if (lightness > 100) lightness = 100;
+
+	// algorithm from: http://www.easyrgb.com/index.php?X=MATH&H=19#text19
+	if (saturation == 0) {
+		red = green = blue = lightness * 255 / 100;
+	} else {
+		if (lightness < 50) {
+			var2 = lightness * (100 + saturation);
+		} else {
+			var2 = ((lightness + saturation) * 100) - (saturation * lightness);
+		}
+		var1 = lightness * 200 - var2;
+		red = h2rgb(var1, var2, (hue < 240) ? hue + 120 : hue - 240) * 255 / 600000;
+		green = h2rgb(var1, var2, hue) * 255 / 600000;
+		blue = h2rgb(var1, var2, (hue >= 120) ? hue - 120 : hue + 240) * 255 / 600000;
+	}
+	return (red << 16) | (green << 8) | blue;
+}
+
+void setup() {
+  pinMode(1, OUTPUT);
+  digitalWrite(1, HIGH);
+  for (int i=0; i<180; i++) {
+    int hue = i * 2;
+    int saturation = 100;
+    int lightness = 30;
+    // pre-compute the 180 rainbow colors
+    rainbowColors[i] = makeColor(hue, saturation, lightness);
+  }
+  digitalWrite(1, LOW);
+  leds.begin();
+}
+
+
+
+// phaseShift is the shift between each row.  phaseShift=0
+// causes all rows to show the same colors moving together.
+// phaseShift=180 causes each row to be the opposite colors
+// as the previous.
+//
+// cycleTime is the number of milliseconds to shift through
+// the entire 360 degrees of the color wheel:
+// Red -> Orange -> Yellow -> Green -> Blue -> Violet -> Red
+//
+void rainbow(int phaseShift, int cycleTime)
+{
+  int color, x, y, wait;
+
+  wait = cycleTime * 1000 / ledsPerStrip;
+  for (color=0; color < 180; color++) {
+    digitalWrite(1, HIGH);
+    for (x=0; x < ledsPerStrip; x++) {
+      for (y=0; y < 8; y++) {
+        int index = (color + x + y*phaseShift/2) % 180;
+        leds.setPixel(x + y*ledsPerStrip, rainbowColors[index]);
+      }
+    }
+    leds.show();
+    digitalWrite(1, LOW);
+    delayMicroseconds(wait);
+  }
+}
+
+void loop() {
+  rainbow(10, 50);
+}
+
+int main() {
+  setup();
+  for(;;) {
+    loop();
+  }
+  return 0;
+}
